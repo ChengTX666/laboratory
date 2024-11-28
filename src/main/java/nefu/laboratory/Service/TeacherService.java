1b665978d4ea0dae43cc5ebac0176e685e12e9f8
ChengTX666 <873661351@qq.com>
2024-11-28 21:28:18 +0800

    start

--- src/main/java/nefu/laboratory/Service/TeacherService.java
+++ src/main/java/nefu/laboratory/Service/TeacherService.java
@@ -1,7 +1,9 @@
 package nefu.laboratory.Service;
 
 import lombok.RequiredArgsConstructor;
+import nefu.laboratory.Dox.Course;
 import nefu.laboratory.Dox.Reservation;
+import nefu.laboratory.Repository.CourseRepository;
 import nefu.laboratory.Repository.ReservationRepository;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +13,12 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TeacherService {
     private final ReservationRepository reservationRepository;
+    private final CourseRepository courseRepository;
 
     public List<Reservation> courseList(String tid){
         return reservationRepository.findByTeacherId(tid);
+    }
+    public List<Course> courseLab(String tid){
+        return courseRepository.findByTeacherId(tid);
     }
 }
