e53cf786281dd19ddc9c2cae5f6c321262c8b690
ChengTX666 <873661351@qq.com>
2024-12-12 10:35:36 +0800

    update

---
+++ src/main/java/nefu/laboratory/dto/WeeksDTO.java
@@ -0,0 +1,24 @@
+package nefu.laboratory.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class WeeksDTO {
+    private String id;
+    private String teacherId;
+    private String laboratoryId;
+    private String laboratoryName;
+    private String courseId;
+    private String courseName;
+    private int period;
+    private int day;
+    private List<Integer> weeks;
+}
