86d4acec787005f5d0c95286f8b599c44b93f7be
ChengTX666 <873661351@qq.com>
2024-12-16 22:36:24 +0800

    update

--- src/main/java/nefu/laboratory/Controller/LoginController.java
+++ src/main/java/nefu/laboratory/Controller/LoginController.java
@@ -11,10 +11,7 @@ import nefu.laboratory.component.JWTComponent;
 import nefu.laboratory.dto.Login;
 import nefu.laboratory.dto.ResultVO;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
